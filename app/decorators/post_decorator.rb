ecd265019a13f4af52bec32cd0f064829e5bddf8
GigaChubinidze <chubinidze46@gmail.com>
2022-07-11 02:05:22 +0400

    minor styling changes

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -20,12 +20,30 @@ class PostDecorator < ApplicationDecorator
       h.button_to(h.t(:delete), model, method: :delete, data: {confirm: "Are you sure?"}, class:"btn btn-primary", style:"position:relative; left: #{lang_detector}px;")
     end
   end
-
+  
+  def zd 
+    "zd"
+  end
   def show_avatar
     if post.user.avatar?
       h.image_tag(model.user.avatar_url(:thumb), style:"width:150px; height:200px; height:auto;")
     else
       h.image_tag("https://cdn3.iconfinder.com/data/icons/flaticons-1/24/flaticon_user-512.png", style:"width:150px; height:200px; height:auto;   border-radius: 50%;")
+    end
+  end
+
+  def delete_if_authora
+    pre_like = post.likes.find { |like| like.user_id == current_user.id}
+    pre_dislike = post.dislikes.find { |dislike| dislike.user_id == current_user.id}
+  
+    disabled_value_like = false
+    if pre_dislike 
+      disabled_value_like = true
+    end
+    if pre_like
+      h.button_to 'Liked', post_like_path(@post, pre_like), method: :delete, :disabled => disabled_value_like, style:"color: white; background-color:red; position:relative; bottom:71px; left: 1170px;", class:"react"
+    else
+      h.button_to 'Like', post_likes_path(@post), method: :post, :disabled => disabled_value_like, style:"color: white; background-color:green; position:relative; bottom:71px; left: 1170px;", class:"react"
     end
   end
 
