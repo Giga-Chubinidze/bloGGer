5da4cd406e9d5518c13c4a61a908cd05fe90aea1
GigaChubinidze <chubinidze46@gmail.com>
2022-07-21 23:30:03 +0400

    tee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,26 +2,26 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.hosts << "localhost:3000"
-  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true 
+  # config.hosts << "localhost:3000"
+  # #config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # #config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.perform_deliveries = true 
   
 
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'localhost:3000',
-    user_name: 'gchubinidze@unisens.ge',
-    password: 'sgjspovlufsufjef',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'localhost:3000',
+  #   user_name: 'gchubinidze@unisens.ge',
+  #   password: 'sgjspovlufsufjef',
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
