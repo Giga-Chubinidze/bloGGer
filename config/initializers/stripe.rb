a11409a37a81462892fdfd3e61f0d4c095e3ba4a
Giga Chubinidze <gchubinidze@unisens.ge>
2022-07-22 09:53:05 +0400

    fixed stripe key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => Rails.application.credentials.stripe_publishable_key,
+    :publishable_key => Rails.application.credentials.dig(:stripe_publishable_key),
   
-    :secret_key      => Rails.application.credentials.stripe_secret_key
+    :secret_key      => Rails.application.credentials.dig(:stripe_secret_key)
    }
-   Stripe.api_key =  Rails.application.credentials.stripe_secret_key
+   Stripe.api_key =  Rails.application.credentials.dig(:stripe_secret_key)
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -50,8 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
@@ -60,8 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
   end
 
   create_table "dislikes", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_dislikes_on_post_id"
@@ -69,8 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
@@ -80,7 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
   create_table "phone_numbers", force: :cascade do |t|
     t.string "country"
     t.string "number"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_phone_numbers_on_user_id"
@@ -99,15 +102,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.boolean "approval_status", default: false
     t.boolean "is_vip", default: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "posts_roles", id: false, force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "role_id"
+    t.bigint "post_id"
+    t.bigint "role_id"
     t.index ["post_id"], name: "index_posts_roles_on_post_id"
     t.index ["role_id"], name: "index_posts_roles_on_role_id"
   end
@@ -115,7 +118,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
@@ -141,8 +144,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_082024) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
+    t.bigint "user_id"
+    t.bigint "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
